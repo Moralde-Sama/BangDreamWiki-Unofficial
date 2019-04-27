a39819ee2e8aa5b8fa00bc124975fb64241e2fec
Moralde-Sama <moralde.sama@gmail.com>
2019-04-27 22:55:03 +0800

    changed icon, removed splashscreen and changed nav icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public appPages = [
     {
       title: 'Events',
       url: '/home',
-      icon: 'home'
+      icon: 'flaticon-event'
     },
     {
       title: 'Bands',
       url: '/list',
-      icon: 'list'
+      icon: 'flaticon-idol'
     }
   ];
 
